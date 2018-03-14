e2cf6886e5bcbdc37eea11f521ca090de92efe11
ghigbie <gehigbie@gmail.com>
2018-03-13 20:23:41 -0600

    Adjust root name in html

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import InternalBlogApp from './InternalBlogApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<InternalBlogApp />, document.getElementById('root'));
+ReactDOM.render(<InternalBlogApp />, document.getElementById('react-container'));
 registerServiceWorker();
